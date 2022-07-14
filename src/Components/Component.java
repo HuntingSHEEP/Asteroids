d530998a77464e24c018447f584b6f1fe03da2d0
HuntingSHEEP <a.glowacki98@gmail.com>
2022-07-14 22:53:28 +0200

    refactor

--- src/Components/Component.java
+++
@@ -1,9 +0,0 @@
-package Components;
-
-import EngineCore.GameObject;
-
-public abstract class Component {
-    public GameObject gameObject; //DO PRZIPISYWANIA TYLKO I WYLACZNIE RODZICA
-    public abstract void awake();
-    public abstract void update(double dt);
-}
